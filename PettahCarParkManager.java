b81217d9637532081a2e93e60c94d244c1c559bb
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-07-30 10:36:14 +0530

    add other vehicle in method implementation

--- PettahCarParkManager.java
+++ PettahCarParkManager.java
@@ -154,15 +154,30 @@ public class PettahCarParkManager implements CarParkManager {
     @Override
     public void deleteVehicle(String IdPlate) {
         for (Vehicle item : listOfVehicle) {
-            //Checking for a particular vehicle with its' plate ID
+            // Checking for a particular vehicle with its' plate ID
             if (item.getIdPlate().equals(IdPlate)) {
                 System.out.println("Vehicle Found.");
-                if (item instanceof Van) {
-                    availableSlots += 2;
+                if (item instanceof Car) {
+                    availableSlots += SPACE_FOR_CAR;
+                    System.out.println("Space cleared after deleting a Car.\nAvailable Slots : " + availableSlots);
+                } else if (item instanceof Van) {
+                    availableSlots += SPACE_FOR_VAN;
                     System.out.println("Space cleared after deleting a Van.\nAvailable Slots : " + availableSlots);
+                } else if (item instanceof MotorBike) {
+                    availableSlots += SPACE_FOR_MOTOR_BIKE;
+                    System.out.println("Space cleared after deleting a Motor Bike.\nAvailable Slots : " + availableSlots);
+                } else if (item instanceof Lorry) {
+                    availableSlots += SPACE_FOR_LORRY;
+                    System.out.println("Space cleared after deleting a Lorry.\nAvailable Slots : " + availableSlots);
+                } else if (item instanceof MiniLorry) {
+                    availableSlots += SPACE_FOR_MINI_LORRY;
+                    System.out.println("Space cleared after deleting a Mini Lorry.\nAvailable Slots : " + availableSlots);
+                } else if (item instanceof Bus) {
+                    availableSlots += SPACE_FOR_BUS;
+                    System.out.println("Space cleared after deleting a Bus.\nAvailable Slots : " + availableSlots);
                 } else {
-                    availableSlots++;
-                    System.out.println("Space cleared after deleting a vehicle.\nAvailable Slots : " + availableSlots);
+                    availableSlots += SPACE_FOR_MINI_BUS;
+                    System.out.println("Space cleared after deleting a Mini Bus.\nAvailable Slots : " + availableSlots);
                 }
             } else {
                 System.out.println("Vehicle not found.");
@@ -170,17 +185,24 @@ public class PettahCarParkManager implements CarParkManager {
         }
     }
 
-
     @Override
     public void printcurrentVehicles() {
         Collections.sort(listOfVehicle, Collections.reverseOrder());
         for (Vehicle item : listOfVehicle) {
-            if (item instanceof Van) {
+            if (item instanceof Car) {
+                System.out.println("Vehicle Type is a Car.");
+            } else if (item instanceof Van) {
                 System.out.println("Vehicle Type is a Van");
             } else if (item instanceof MotorBike) {
-                System.out.println("Vehicle Type is a MotorBike");
+                System.out.println("Vehicle Type is a Motor Bike");
+            } else if (item instanceof Lorry) {
+                System.out.println("Vehicle Type is a Lorry");
+            } else if (item instanceof MiniLorry) {
+                System.out.println("Vehicle Type is a Mini Lorry");
+            } else if (item instanceof Bus) {
+                System.out.println("Vehicle Type is a Bus");
             } else {
-                System.out.println("Vehicle Type is a Car.");
+                System.out.println("Vehicle Type is a Mini Bus");
             }
             System.out.println("******************");
             System.out.println("ID Plate : " + item.getIdPlate());
@@ -191,7 +213,7 @@ public class PettahCarParkManager implements CarParkManager {
 
     @Override
     public void printLongestPark() {
-        //sort to the ascending order
+        // Sort to the ascending order
         Collections.sort(listOfVehicle);
         System.out.println("The longest parked vehicle is : ");
         System.out.println("................................................");
@@ -199,9 +221,17 @@ public class PettahCarParkManager implements CarParkManager {
         if (listOfVehicle.get(0) instanceof Car) {
             System.out.println("Vehicle Type is a Car.");
         } else if (listOfVehicle.get(0) instanceof Van) {
-            System.out.println("Vehicle Type is a Van.");
+            System.out.println("Vehicle Type is a Van");
+        } else if (listOfVehicle.get(0) instanceof MotorBike) {
+            System.out.println("Vehicle Type is a Motor Bike");
+        } else if (listOfVehicle.get(0) instanceof Lorry) {
+            System.out.println("Vehicle Type is a Lorry");
+        } else if (listOfVehicle.get(0) instanceof MiniLorry) {
+            System.out.println("Vehicle Type is a Mini Lorry");
+        } else if (listOfVehicle.get(0) instanceof Bus) {
+            System.out.println("Vehicle Type is a Bus");
         } else {
-            System.out.println("Vehicle Type is a MotorBike.");
+            System.out.println("Vehicle Type is a Mini Bus");
         }
         System.out.println("Parked Time : " + listOfVehicle.get(0).getEntryDate().getHours() + ":" + listOfVehicle.get(0).getEntryDate().getMinutes() + ":" + listOfVehicle.get(0).getEntryDate().getSeconds());
         System.out.println("Parked Date  : " + listOfVehicle.get(0).getEntryDate().getDate() + "/" + listOfVehicle.get(0).getEntryDate().getMonth() + "/" + listOfVehicle.get(0).getEntryDate().getYear());
@@ -209,7 +239,7 @@ public class PettahCarParkManager implements CarParkManager {
 
     @Override
     public void printLatestPark() {
-        // sort to the descending order
+        // Sort to the descending order
         Collections.sort(listOfVehicle, Collections.reverseOrder());
         System.out.println("The latest parked vehicle is : ");
         System.out.println("..............................................");
@@ -217,30 +247,42 @@ public class PettahCarParkManager implements CarParkManager {
         if (listOfVehicle.get(0) instanceof Car) {
             System.out.println("Vehicle Type is a Car.");
         } else if (listOfVehicle.get(0) instanceof Van) {
-            System.out.println("Vehicle Type is a Van.");
+            System.out.println("Vehicle Type is a Van");
+        } else if (listOfVehicle.get(0) instanceof MotorBike) {
+            System.out.println("Vehicle Type is a Motor Bike");
+        } else if (listOfVehicle.get(0) instanceof Lorry) {
+            System.out.println("Vehicle Type is a Lorry");
+        } else if (listOfVehicle.get(0) instanceof MiniLorry) {
+            System.out.println("Vehicle Type is a Mini Lorry");
+        } else if (listOfVehicle.get(0) instanceof Bus) {
+            System.out.println("Vehicle Type is a Bus");
         } else {
-            System.out.println("Vehicle Type is a MotorBike.");
+            System.out.println("Vehicle Type is a Mini Bus");
         }
         System.out.println("Parked Time : " + listOfVehicle.get(0).getEntryDate().getHours() + ":" + listOfVehicle.get(0).getEntryDate().getMinutes() + ":" + listOfVehicle.get(0).getEntryDate().getSeconds());
         System.out.println("Parked Date  : " + listOfVehicle.get(0).getEntryDate().getDate() + "/" + listOfVehicle.get(0).getEntryDate().getMonth() + "/" + listOfVehicle.get(0).getEntryDate().getYear());
     }
 
-
     @Override
     public void printVehicleByDay(DateTime givenDate) {
         for (Vehicle item : listOfVehicle) {
             if (givenDate.getYear() == item.getEntryDate().getYear() && givenDate.getMonth() == item.getEntryDate().getMonth() && givenDate.getDate() == item.getEntryDate().getDate()) {
-
                 System.out.println("ID Plate : " + item.getIdPlate());
-
                 System.out.println("Parked Date and Time : " + item.getEntryDate().getDate() + "/" + item.getEntryDate().getMonth() + "/" + item.getEntryDate().getHours() + "-" + item.getEntryDate().getHours() + ":" + item.getEntryDate().getMinutes() + ":" + item.getEntryDate().getYear());
-
-                if (item instanceof Van) {
+                if (item instanceof Car) {
+                    System.out.println("Vehicle Type is a Car.");
+                } else if (item instanceof Van) {
                     System.out.println("Vehicle Type is a Van");
                 } else if (item instanceof MotorBike) {
-                    System.out.println("Vehicle Type is a Motor Bike.");
+                    System.out.println("Vehicle Type is a Motor Bike");
+                } else if (item instanceof Lorry) {
+                    System.out.println("Vehicle Type is a Lorry");
+                } else if (item instanceof MiniLorry) {
+                    System.out.println("Vehicle Type is a Mini Lorry");
+                } else if (item instanceof Bus) {
+                    System.out.println("Vehicle Type is a Bus");
                 } else {
-                    System.out.println("Vehicle Type is a Car.");
+                    System.out.println("Vehicle Type is a Mini Bus");
                 }
                 System.out.println("--------------------------");
                 System.out.println("\n");
@@ -251,24 +293,43 @@ public class PettahCarParkManager implements CarParkManager {
     @Override
     public void printVehiclePercentage() {
         int numCars = 0;
-        int numBikes = 0;
         int numVans = 0;
+        int numBikes = 0;
+        int numLorry = 0;
+        int numMiniLorry = 0;
+        int numBus = 0;
+        int numMiniBus = 0;
         for (Vehicle item : listOfVehicle) {
             if (item instanceof Car) {
                 numCars++;
+            } else if (item instanceof Van) {
+                numVans++;
             } else if (item instanceof MotorBike) {
                 numBikes++;
+            } else if (item instanceof Lorry) {
+                numLorry++;
+            } else if (item instanceof MiniLorry) {
+                numMiniLorry++;
+            } else if (item instanceof Bus) {
+                numBus++;
             } else {
-                numVans++;
+                numMiniBus++;
             }
         }
         double carPercentage = (numCars / listOfVehicle.size()) * 100;
-        double bikePercentage = (numBikes / listOfVehicle.size()) * 100;
         double vanPercentage = (numVans / listOfVehicle.size()) * 100;
-
-        System.out.printf("Car Percentage is : %.f ", carPercentage);
-        System.out.printf("\nBike Percentage is : %.f ", bikePercentage);
-        System.out.printf("\nVan Percentage is : %.f ", vanPercentage);
+        double bikePercentage = (numBikes / listOfVehicle.size()) * 100;
+        double lorryPercentage = (numLorry / listOfVehicle.size()) * 100;
+        double miniLorryPercentage = (numMiniLorry / listOfVehicle.size()) * 100;
+        double busPercentage = (numBus / listOfVehicle.size()) * 100;
+        double miniBusPercentage = (numMiniBus / listOfVehicle.size()) * 100;
+        System.out.printf("Car percentage is : %.f \n", carPercentage);
+        System.out.printf("Van percentage is : %.f \n", vanPercentage);
+        System.out.printf("Motor Bike percentage is : %.f \n", bikePercentage);
+        System.out.printf("Lorry percentage is : %.f \n", lorryPercentage);
+        System.out.printf("Mini Lorry percentage is : %.f \n", miniLorryPercentage);
+        System.out.printf("Bus percentage is : %.f \n", busPercentage);
+        System.out.printf("Mini Bus percentage is : %.f \n", miniBusPercentage);
         System.out.println("\n");
     }
 
@@ -279,32 +340,29 @@ public class PettahCarParkManager implements CarParkManager {
         for (Vehicle item : listOfVehicle) {
             if (item.getIdPlate().equals(plateID)) {
                 System.out.println("Vehicle found.");
-                //vehicle parked time
-                System.out.println("Parked Time : " + item.getEntryDate().getDate() + "/" + item.getEntryDate().getMonth() + "/" + item.getEntryDate().getDate() + "-" + item.getEntryDate().getHours() + ":" + item.getEntryDate().getMinutes() + ":" + item.getEntryDate().getSeconds());
-                //making the charges
+                // Vehicle parked time
+                System.out.println("Parked time : " + item.getEntryDate().getDate() + "/" + item.getEntryDate().getMonth() + "/" + item.getEntryDate().getDate() + "-" + item.getEntryDate().getHours() + ":" + item.getEntryDate().getMinutes() + ":" + item.getEntryDate().getSeconds());
+                // Making the charges
                 found = true;
                 DateTime entryDateTime = item.getEntryDate();
                 int differenceInSeconds = currentTime.compareTo(entryDateTime);
                 double differenceInHours = differenceInSeconds / (60.0 * 60.0);
-
                 double dayCharge = 0;
                 double hourCharge = 0;
                 double totalCost = 0;
                 double days = differenceInHours / 24;
-
                 if (days > 1) {
                     dayCharge = maxCharge;
                 }
                 if (differenceInHours >= 3) {
                     double additional = (differenceInHours - addFromthisHour);
                     hourCharge = (additional * addCharge) + (addFromthisHour * chargePerHour);
-                    System.out.printf("hour Charge : %.2f", hourCharge);
+                    System.out.printf("Hour charge : %.2f", hourCharge);
                 } else if (differenceInHours < 1) {
                     hourCharge = chargePerHour;
                 } else {
                     hourCharge = (differenceInHours * chargePerHour);
                 }
-
                 totalCost = dayCharge + hourCharge;
                 BigDecimal vehicleCharge = new BigDecimal(totalCost);
                 System.out.printf("Total charge for the vehicle is LKR %.2f", vehicleCharge);
@@ -316,6 +374,4 @@ public class PettahCarParkManager implements CarParkManager {
         }
         return charges;
     }
-
-
 }
