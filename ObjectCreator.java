2c2bb912a6f36bd60669c6496b56d71912372d9b
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-07-30 10:58:50 +0530

    fix object creator bug

--- ObjectCreator.java
+++ ObjectCreator.java
@@ -47,28 +47,28 @@ public class ObjectCreator {
                 double lorryCargoCapacity = sc.nextDouble();
                 System.out.println("Enter the height : ");
                 double lorryHeight = sc.nextDouble();
-                new Lorry(plateID, brand, model, entryTime, lorryCargoCapacity, lorryHeight);
+                obj = new Lorry(plateID, brand, model, entryTime, lorryCargoCapacity, lorryHeight);
                 break;
             case MiniLorry:
                 System.out.println("Enter the Cargo Capacity : ");
                 double miniLorryCargoCapacity = sc.nextDouble();
                 System.out.println("Enter the height : ");
                 double miniLorryHeight = sc.nextDouble();
-                new MiniLorry(plateID, brand, model, entryTime, miniLorryCargoCapacity, miniLorryHeight);
+                obj =new MiniLorry(plateID, brand, model, entryTime, miniLorryCargoCapacity, miniLorryHeight);
                 break;
             case Bus:
                 System.out.println("Enter the number of seats : ");
                 int busSeats = sc.nextInt();
                 System.out.println("Enter the height : ");
                 double busHeight = sc.nextDouble();
-                new Bus(plateID, brand, model, entryTime, busSeats, busHeight);
+                obj = new Bus(plateID, brand, model, entryTime, busSeats, busHeight);
                 break;
             case MiniBus:
                 System.out.println("Enter the number of seats : ");
                 int miniBusSeats = sc.nextInt();
                 System.out.println("Enter the height : ");
                 double miniBusHeight = sc.nextDouble();
-                new MiniBus(plateID, brand, model, entryTime, miniBusSeats, miniBusHeight);
+                obj = new MiniBus(plateID, brand, model, entryTime, miniBusSeats, miniBusHeight);
                 break;
             default:
                 System.out.println("Invalid Choice");
